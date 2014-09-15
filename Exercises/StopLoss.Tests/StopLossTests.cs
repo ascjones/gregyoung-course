d4029cd7a71a2121137fcf961249787b49f6f7a1
Andrew Jones <ascjones@gmail.com>
2014-09-15 13:10:34 +0100

    first few tests

---
+++ Exercises/StopLoss.Tests/StopLossTests.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace StopLoss.Tests
+{
+    [TestFixture]
+    public class StopLossTests
+    {
+        private FakeMessageBus bus;
+        private StopLossManager stopLossManager;
+
+        [SetUp]
+        public void SetUp()
+        {
+            bus = new FakeMessageBus();
+            stopLossManager = new StopLossManager(bus);   
+        }
+
+        [Test]
+        public void WhenPositionAcquiredThenTargetUpdated()
+        {
+            var  positionAcquired = new PositionAcquired(1.0M);
+            stopLossManager.Consume(positionAcquired);
+
+            var events = bus.GetMessages();
+            var targetUpdated = events.First() as TargetUpdated;
+            Assert.That(targetUpdated, Is.Not.Null, "Should be TargetUpdated");
+            Assert.That(targetUpdated.TargetPrice, Is.EqualTo(0.9M));
+        }
+
+        [Test]
+        public void WhenPriceUpdatedWithinTenSecondsThenRaisesTwoSendInX()
+        {
+            var priceUpdated = new PriceUpdated {Price = 1.5M};
+            stopLossManager.Consume(priceUpdated);
+
+            var events = bus.GetMessages();
+            Assert.That(events, Has.Length.EqualTo(2));
+            var sendToMeInX1 = events.First() as SendToMeInX<PriceUpdated>;
+            var sendToMeInX2 = events.Last() as SendToMeInX<PriceUpdated>;
+
+
+            Assert.That(sendToMeInX1, Is.Not.Null);
+            Assert.That(sendToMeInX1.Message, Is.EqualTo(priceUpdated));
+            Assert.That(sendToMeInX1.X, Is.EqualTo(10.0M));
+
+            Assert.That(sendToMeInX2, Is.Not.Null);
+            Assert.That(sendToMeInX2.Message, Is.EqualTo(priceUpdated));
+            Assert.That(sendToMeInX2.X, Is.EqualTo(7.0M));
+        }
+
+        [Test]
+        public void WhenPriceSustainedForLongerThan10SecondsThenTargetUpdated()
+        {
+            var positionAcquired = new PositionAcquired(1.0M);
+            stopLossManager.Consume(positionAcquired);
+
+            var priceUpdated = new PriceUpdated { Price = 1.5M };
+            stopLossManager.Consume(priceUpdated);
+            var sendToMeInX = new SendToMeInX<PriceUpdated>(10.0M, priceUpdated);
+            stopLossManager.Consume(sendToMeInX);
+
+            var targetUpdated = bus.GetLastMessage<TargetUpdated>();
+
+            Assert.That(targetUpdated, Is.Not.Null);
+            Assert.That(targetUpdated.TargetPrice, Is.EqualTo(1.4M));
+        }
+
+        [Test]
+        public void WhenPriceNotSustainedForLongerThan10SecondsThenTargetNotUpdated()
+        {
+            var positionAcquired = new PositionAcquired(1.0M);
+            stopLossManager.Consume(positionAcquired);
+
+            var priceUpdated = new PriceUpdated { Price = 0.8M };
+            stopLossManager.Consume(priceUpdated);
+
+            bus.Clear();
+
+            priceUpdated = new PriceUpdated { Price = 1.5M };
+            var sendToMeInX = new SendToMeInX<PriceUpdated>(10.0M, priceUpdated);
+            stopLossManager.Consume(sendToMeInX);
+
+            var targetUpdated = bus.GetLastMessage<TargetUpdated>();
+
+            Assert.That(targetUpdated, Is.Null);
+        }
+
+        [Test]
+        public void WhenPriceGoesUpAndThenDownThenTakesTheMinimumSustainedIncrease()
+        {
+            var positionAcquired = new PositionAcquired(1.0M);
+            stopLossManager.Consume(positionAcquired);
+
+            var priceUpdated = new PriceUpdated { Price = 1.5M };
+            stopLossManager.Consume(priceUpdated);
+
+            priceUpdated = new PriceUpdated { Price = 1.2M };
+            stopLossManager.Consume(priceUpdated);
+
+            bus.Clear();
+
+            priceUpdated = new PriceUpdated { Price = 1.5M };
+            var sendToMeInX = new SendToMeInX<PriceUpdated>(10.0M, priceUpdated);
+            stopLossManager.Consume(sendToMeInX);
+
+            var targetUpdated = bus.GetLastMessage<TargetUpdated>();
+
+            Assert.That(targetUpdated, Is.Not.Null);
+            Assert.That(targetUpdated.TargetPrice, Is.EqualTo(1.1M));
+        }
+
+        [Test]
+        public void WhenPriceGoesDownAndIsSustainedForLongerThan7SecondsThenTriggerStopLoss()
+        {
+            var positionAcquired = new PositionAcquired(1.0M);
+            stopLossManager.Consume(positionAcquired);
+
+            var priceUpdated = new PriceUpdated { Price = 0.89M };
+            stopLossManager.Consume(priceUpdated);
+
+            var sendToMeInX = new SendToMeInX<PriceUpdated>(7.0M, priceUpdated);
+            stopLossManager.Consume(sendToMeInX);
+
+            var stopLossTriggered = bus.GetLastMessage<StopLossTriggered>();
+
+            Assert.That(stopLossTriggered, Is.Not.Null);
+        }
+    }
+}
---
+++ Exercises/StopLoss/StopLossManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace StopLoss
+{
+    public class StopLossManager
+    {
+        private readonly IMessageBus _bus;
+        private readonly List<decimal> priceUpdates = new List<decimal>();
+
+        private decimal initialPosition;
+        private decimal currentStopTarget;
+
+        public StopLossManager(IMessageBus bus)
+        {
+            _bus = bus;
+        }
+
+        public void Consume(PositionAcquired positionAcquired)
+        {
+            initialPosition = positionAcquired.Price;  
+            RaiseTargetUpdated(positionAcquired.Price);
+        }
+
+        public void Consume(PriceUpdated priceUpdated)
+        {
+            priceUpdates.Add(priceUpdated.Price);
+            
+            _bus.Publish(new SendToMeInX<PriceUpdated>(10.0M, priceUpdated));
+            _bus.Publish(new SendToMeInX<PriceUpdated>(7.0M, priceUpdated));
+        }
+
+        public void Consume(SendToMeInX<PriceUpdated> msg)
+        {
+            var minPrice = priceUpdates.Any() ? priceUpdates.Min() : initialPosition;
+            if (minPrice > currentStopTarget)
+            {
+                RaiseTargetUpdated(minPrice);   
+            }
+
+            var maxPrice = priceUpdates.Any() ? priceUpdates.Max() : initialPosition;
+            if (maxPrice < currentStopTarget)
+            {
+                _bus.Publish(new StopLossTriggered());
+            }
+        }
+
+        private void RaiseTargetUpdated(decimal price)
+        {
+            currentStopTarget = price - StopLossThreshold;
+            var targetUpdated = new TargetUpdated(currentStopTarget);
+            _bus.Publish(targetUpdated); 
+        }
+
+        public const decimal StopLossThreshold = 0.1m;
+    }
+
+    public interface IMessageBus
+    {
+        void Publish(IMessage msg);
+    }
+}
